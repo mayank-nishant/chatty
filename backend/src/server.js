1919124d46660f198ff109da9cf0e3b959a90df7
mayank-nishant <mail.nishantmayank@gmail.com>
2026-05-29 11:36:29 +0530

    fix paths

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,12 +15,10 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => console.log("Server is running on port : " + PORT));
