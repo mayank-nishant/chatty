cc6dc34f9e045ea25f1146565fee3456ed515922
mayank-nishant <mail.nishantmayank@gmail.com>
2026-05-29 12:17:28 +0530

    fix deployed frontend path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import fs from "fs";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,25 +11,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// Absolute path setup
 const __dirname = path.resolve();
 
-const frontendPath = path.join(__dirname, "frontend/dist");
-const indexPath = path.join(frontendPath, "index.html");
+// Frontend build path
+const frontendPath = path.join(__dirname, "../frontend/dist");
 
-console.log("ROOT:", __dirname);
-console.log("FRONTEND PATH:", frontendPath);
-console.log("INDEX PATH:", indexPath);
-console.log("INDEX EXISTS:", fs.existsSync(indexPath));
-
+// Serve frontend static files
 app.use(express.static(frontendPath));
 
+// Catch-all route for React/Vite frontend
 app.get("/{*any}", (req, res) => {
-  res.sendFile(indexPath);
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
+// Start server
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server is running on port: ${PORT}`);
 });
