61405c7a6428716d79c91489cdb9b460634bb34a
mayank-nishant <mail.nishantmayank@gmail.com>
2026-05-29 11:42:44 +0530

    fix production frontend path

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,17 +8,20 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+const __dirname = path.resolve();
 
-app.get("/{*any}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log("Server is running on port : " + PORT));
