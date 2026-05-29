4b6234bc80d35dc8a1f0f992d44238f3c17f305f
mayank-nishant <mail.nishantmayank@gmail.com>
2026-05-29 12:13:39 +0530

    debug frontend paths

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import fs from "fs";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -16,12 +17,20 @@ app.use("/api/messages", messageRoutes);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+const frontendPath = path.join(__dirname, "frontend/dist");
+const indexPath = path.join(frontendPath, "index.html");
+
+console.log("ROOT:", __dirname);
+console.log("FRONTEND PATH:", frontendPath);
+console.log("INDEX PATH:", indexPath);
+console.log("INDEX EXISTS:", fs.existsSync(indexPath));
+
+app.use(express.static(frontendPath));
 
 app.get("/{*any}", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
+  console.log("Server running on port", PORT);
 });
