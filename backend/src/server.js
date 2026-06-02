5d071c32224aa2847fcea20544d3ecd65f5fe2f0
mayank-nishant <mail.nishantmayank@gmail.com>
2026-06-02 14:14:53 +0530

    Deployment Fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,44 +5,75 @@ import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
+
 const isProduction = process.env.NODE_ENV === "production";
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(
+  express.json({
+    limit: "10mb",
+  }),
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10mb",
+  }),
+);
+
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRoutes);
+
 app.use("/api/messages", messageRoutes);
 
 app.get("/api/health", (_, res) => {
-  res.status(200).json({ success: true, message: "Server is running." });
+  res.status(200).json({
+    success: true,
+    message: "Server is running.",
+  });
 });
 
 if (isProduction) {
   const frontendPath = path.join(__dirname, "../frontend/dist");
+
   app.use(express.static(frontendPath));
-  app.get("*", (_, res) => res.sendFile(path.join(frontendPath, "index.html")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
+
     server.listen(PORT, () => {
       console.info(`
 ========================================
   Server running successfully
   PORT: ${PORT}
   MODE: ${process.env.NODE_ENV}
-========================================`);
+========================================
+      `);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
+
     process.exit(1);
   }
 };
